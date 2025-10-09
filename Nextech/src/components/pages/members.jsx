514d4258b75e62dd4cb7b93dea16d6000fef8a11
Ayoubslh <salhiayoub801@gmail.com>
2025-10-10 00:07:10 +0100

    responsive  memebr cards

--- Nextech/src/components/pages/members.jsx
+++ Nextech/src/components/pages/members.jsx
@@ -15,7 +15,7 @@ export default function Members() {
                     </p>
                 </div>
 
-             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 pb-20">
+             <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 pb-20">
   {members.map((member) => (
     <MemberCard key={member.name} member={member} />
   ))}
